25dccbea99301299942613d802bd6af3f8a02c5a
vietnd5 <vietnd5@gmail.com>
2018-04-03 14:25:35 +0700

    redux 104  thuc hanh

--- 099_redux/src/App.js
+++ 099_redux/src/App.js
@@ -21,13 +21,24 @@ class App extends Component {
           return {...state,num:[...state.num,action.newItem]}       
           break;
 
+        case "DELETE":
+          return {...state,num:state.num.filter((value,i) => i != action.number  )}       
+          break;
+
       
         default:
+          return state; 
           break;
       }
-      return state; 
+     
     }
     var store1 = redux.createStore(reducer1);
+    store1.subscribe(() => {
+      var str = JSON.stringify(store1.getState());
+      document.getElementById('tb').innerHTML = str; 
+      return str;
+    })
+
     console.log(store1.getState());
     store1.dispatch({type:"CHANGE_EDIT_STATUS"})
     console.log(store1.getState());
@@ -38,14 +49,18 @@ class App extends Component {
     })
     console.log(store1.getState());
 
-
+    store1.dispatch({
+      type:"DELETE",
+      number:0
+    })
+    console.log(store1.getState());
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">str</h1>
         </header>
-        <p className="App-intro">
+        <p className="App-intro" id="tb">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
